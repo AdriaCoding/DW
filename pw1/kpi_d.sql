e8f1ffa8e67e37082747cf15509c5a5445df9dc5
blas <adrialisa99@gmail.com>
2024-11-02 13:31:56 +0100

    realized you cannot agregate rates

--- pw1/kpi_d.sql
+++ pw1/kpi_d.sql
@@ -1,41 +1,46 @@
-CREATE MATERIALIZED VIEW Logbook_summary
+CREATE MATERIALIZED VIEW Marep_rate_summary
 BUILD IMMEDIATE
 REFRESH FORCE ON DEMAND
 ENABLE QUERY REWRITE
 AS
 SELECT 
-    L.reporteur_id
-    L.airport_id
-    DS.aircraftRegistration
-    DS.model
+    L.airport_id,
+    DS.aircraftRegistration,
+    DS.model,
+    L.maintenance_logbook_count,
+    DS.total_FH,
+    DS.total_FO,
+    
+    -- Maintenance Report Rate per Hour (MRRh) = 1000 * Maintenance_Logbook_Count / Total_Flight_Hours
     CASE 
-        WHEN NVL(DS.total_FH), 0) > 0
+        WHEN NVL(DS.total_FH, 0) > 0
         THEN 1000 * NVL(L.maintenance_logbook_count, 0) / DS.total_FH
         ELSE 0
-    END AS MRRh
+    END AS MRRh,
+
+    -- Maintenance Report Rate per Cycle (MRRc) = 100 * Maintenance_Logbook_Count / Total_Departures
     CASE 
         WHEN NVL(DS.total_FO, 0) > 0
         THEN 100 * NVL(L.maintenance_logbook_count, 0) / DS.total_FO
         ELSE 0
-    END AS MRRh
+    END AS MRRc,
 FROM
 (
     SELECT
-        Logbook.aircraftRegistration
-        LogBook.airport_id
-        LogBook.reporteur_id
+        Logbook.aircraftRegistration,
+        LogBook.airport_id,
+        COUNT(CASE WHEN reporteur_class = 'MAREP' THEN 1 END) AS maintenance_logbook_count,
     FROM LogBook
-    WHERE reporteur_class = 'MAREP'
+    GROUP BY LogBook.airport_id, LogBook.aircraftRegistration
 ) AS L
 INNER JOIN 
 (
     SELECT
-        SUM(FH) AS total_FH
-        SUM(TO) AS total_FO
-        aircraftRegistration
-        model
+        SUM(FH) AS total_FH,
+        SUM(TO) AS total_FO,
+        aircraftRegistration,
+        model,
     FROM Daily_Summary
-    GROUP BY aircraftRegistration
+    GROUP BY DS.aircraftRegistration
 ) AS DS 
-ON DS.aircraftRegistration = L.aircraftRegistration
-AND 
+ON DS.aircraftRegistration = L.aircraftRegistration;
